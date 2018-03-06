94b9d62acc26db5328a6998931200dedb5162a91
kemp <572068511@qq.com>
2018-03-06 17:10:10 +0800

    初始化项目版本

---
+++ Desktop/大话数据结构源代码缺1.2章/PlayWithDataStructureSourceCode/第3章线性表/xianxingbiao.c
@@ -0,0 +1,11 @@
+#define OK 1 
+ #define ERROR -1
+ #define MAX_SIZE 100
+ 
+ typedef int Status;
+ typedef int ElemType;
+ typedef struct sqlist
+ {
+ 	ElemType Elem_array[MAX_SIZE]
+ 	int length;
+ } sqlist
